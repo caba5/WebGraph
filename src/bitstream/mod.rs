c587d8c546b99c475044701e4312a68d78099eb1
caba5 <elabalero@gmail.com>
2023-05-24 11:35:18 +0200

    Updated 'refill' and created 'read'

--- src/bitstream/mod.rs
+++ src/bitstream/mod.rs
@@ -1,43 +1,68 @@
-pub struct BitStream<'a> {
+use std::iter;
+
+pub struct BitStream {
     buffer_size: u32,
-    input_stream: &'a dyn Iterator<Item = u32>,
-    read_bits: u32,
-    current: u32,
-    buffer: Vec<u8>,
-    fill: u32,
-    pos: usize,
-    avail: u32,
-    position: u32,
+    // input_stream: &'a dyn Iterator<Item = u32>,
+    read_bits: u32,  // Number of read bits
+    current: u32,  // Current bit buffer: the lowest 'fill' bits represent the current content
+    buffer: Vec<u8>,  // Stream buffer
+    fill: u32,  // Current number of bits in the bit buffer ('current')
+    pos: usize,  // Current position in the byte buffer ('buffer')
+    avail: u32,  // Current number of available bytes in the byte buffer 
+    position: usize,  // Current position of the first byte in the byte buffer
 }
 
-impl<'a> BitStream<'a> {
-    pub fn new(input_stream: &dyn Iterator<Item = u32>, buffer_size: u32) -> BitStream {
-        BitStream {
-            buffer_size: if buffer_size > 0 {buffer_size} else {0},
-            input_stream,
+impl BitStream {
+    pub fn new(byte_arr: &[u8]) -> BitStream {
+        if byte_arr.len() > 0 {
+            return BitStream {
+                buffer_size: 8 * 1024,
+                // input_stream: iter::empty::<u32>(),
+                read_bits: 0,
+                current: 0,
+                buffer: Vec::from(byte_arr),
+                fill: 0,
+                pos: 0,
+                avail: byte_arr.len() as u32,
+                position: 0
+            };
+        }
+        BitStream { // TODO: is it necessary?
+            buffer_size: 8 * 1024,
             read_bits: 0,
             current: 0,
-            buffer: Vec::with_capacity(buffer_size as usize),
+            buffer: Vec::new(),
             fill: 0,
             pos: 0,
             avail: 0,
             position: 0
         }
+        
     }
 
+    /// Reads the next byte from the stream.
     pub fn read(&self) -> u32 {
-        todo!() // https://github.com/vigna/dsiutils/blob/master/src/it/unimi/dsi/io/InputBitStream.java
+        if self.avail == 0 {
+            self.position += self.pos;
+            self.pos = 0;
+        }
+
+        self.avail -= 1;
+        let read_val = self.buffer[self.pos] & 0xFF;
+        self.pos += 1;
+        read_val as u32
     }
 
+    /// Feeds 16 more bits into [current](BitStream::current).
     pub fn refill(&self) -> u32 {
         assert!(self.fill < 16);
 
         if self.avail > 1 {
             self.avail -= 2;
-            self.current <<= 16 | (self.buffer[self.pos] & 0xFF) << 8;
-            self.pos += 1;
-            self.current |= self.buffer[self.pos] as u32 & 0xFF;
-            self.pos += 1;
+            self.current = self.current << 16 | ((self.buffer[self.pos] & 0xFF) << 8 | self.buffer[self.pos + 1] & 0xFF) as u32;
+            self.pos += 2;
+            self.fill += 16;
+            return self.fill;
         }
 
         self.current = (self.current << 8) | self.read();
@@ -47,4 +72,6 @@ impl<'a> BitStream<'a> {
 
         self.fill
     }
+
+    // TODO: https://github.com/vigna/dsiutils/blob/master/src/it/unimi/dsi/io/InputBitStream.java#L501
 }
