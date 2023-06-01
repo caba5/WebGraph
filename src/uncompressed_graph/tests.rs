e40b4bcb974acb0e02618d4f4e7c7e5328b9c4bd
caba5 <elabalero@gmail.com>
2023-06-01 11:24:41 +0200

    Tested iteration on various successors

--- src/uncompressed_graph/tests.rs
+++ src/uncompressed_graph/tests.rs
@@ -45,3 +45,62 @@ fn test_graph_building_bin_nodes() {
     let uncompressed_graph = build_graph_bin::<u32>();
     assert_eq!(uncompressed_graph.num_nodes(), 100000);
 }
+
+#[test]
+fn test_iterate_on_first_successors() {
+    let uncompressed_graph = build_graph_bin::<u32>();
+    let mut it = uncompressed_graph.successors(0);
+
+    assert_eq!(it.next().unwrap(), 1);
+    assert_eq!(it.next().unwrap(), 4);
+    assert_eq!(it.next().unwrap(), 5);
+    assert_eq!(it.next().unwrap(), 6);
+    assert_eq!(it.next().unwrap(), 7);
+    assert_eq!(it.next().unwrap(), 8);
+    assert_eq!(it.next().unwrap(), 13);
+    assert_eq!(it.next().unwrap(), 38);
+    assert_eq!(it.next().unwrap(), 48);
+    assert_eq!(it.next().unwrap(), 154);
+    assert!(it.next().is_none());
+}
+
+#[test]
+fn test_iterate_on_mid_successors() {
+    let uncompressed_graph = build_graph_bin::<u32>();
+    let mut it = uncompressed_graph.successors(6);
+
+    assert_eq!(it.next().unwrap(), 0);
+    assert_eq!(it.next().unwrap(), 1);
+    assert_eq!(it.next().unwrap(), 4);
+    assert_eq!(it.next().unwrap(), 5);
+    assert_eq!(it.next().unwrap(), 6);
+    assert_eq!(it.next().unwrap(), 7);
+    assert_eq!(it.next().unwrap(), 8);
+    assert_eq!(it.next().unwrap(), 13);
+    assert_eq!(it.next().unwrap(), 38);
+    assert_eq!(it.next().unwrap(), 84821);
+    assert!(it.next().is_none());
+}
+
+#[test]
+fn test_iterate_on_end_successors() {
+    let uncompressed_graph = build_graph_bin::<u32>();
+    let last_node = (uncompressed_graph.n - 1) as u32;  // n-1 since it is the last node
+    let mut it = uncompressed_graph.successors(last_node);
+
+    assert_eq!(it.next().unwrap(), 99979);
+    assert_eq!(it.next().unwrap(), 99980);
+    assert_eq!(it.next().unwrap(), 99981);
+    assert_eq!(it.next().unwrap(), 99982);
+    assert_eq!(it.next().unwrap(), 99983);
+    assert_eq!(it.next().unwrap(), 99984);
+    assert_eq!(it.next().unwrap(), 99985);
+    assert_eq!(it.next().unwrap(), 99986);
+    assert_eq!(it.next().unwrap(), 99987);
+    assert_eq!(it.next().unwrap(), 99988);
+    assert_eq!(it.next().unwrap(), 99989);
+    assert_eq!(it.next().unwrap(), 99990);
+    assert_eq!(it.next().unwrap(), 99998);
+    assert_eq!(it.next().unwrap(), 99999);
+    assert!(it.next().is_none());
+}
