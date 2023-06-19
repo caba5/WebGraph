c3030e61c33ef7fab1ba815db58e4f94b5bd3330
caba5 <elabalero@gmail.com>
2023-06-19 10:22:40 +0200

    Refactored iterator tests

--- src/webgraph/tests.rs
+++ src/webgraph/tests.rs
@@ -52,63 +52,39 @@ fn test_outdegree_out_of_bounds() {
 
 #[test]
 fn test_iteration_on_successors_of_first_node() {
-    let mut graph = build_graph();
-    let mut succ_it = graph.successors(0).unwrap();
+    let graph = build_graph();
+    let succ_it = graph.successors(0).unwrap();
 
-    assert_eq!(succ_it.next().unwrap(), 1);
-    assert_eq!(succ_it.next().unwrap(), 4);
-    assert_eq!(succ_it.next().unwrap(), 5);
-    assert_eq!(succ_it.next().unwrap(), 6);
-    assert_eq!(succ_it.next().unwrap(), 7);
-    assert_eq!(succ_it.next().unwrap(), 8);
-    assert_eq!(succ_it.next().unwrap(), 13);
-    assert_eq!(succ_it.next().unwrap(), 38);
-    assert_eq!(succ_it.next().unwrap(), 48);
-    assert_eq!(succ_it.next().unwrap(), 154);
-    assert_eq!(succ_it.next(), None);
+    let correct = vec![1, 4, 5, 6, 7, 8, 13, 38, 48, 154].into_iter();
+
+    assert!(succ_it.eq(correct));
 }
 
 #[test]
 fn test_iteration_on_successors_of_mid_node() {
-    let mut graph = build_graph();
-    let mut succ_it = graph.successors(1).unwrap();
+    let graph = build_graph();
+    let succ_it = graph.successors(1).unwrap();
 
-    assert_eq!(succ_it.next().unwrap(), 10);
-    assert_eq!(succ_it.next().unwrap(), 38);
-    assert_eq!(succ_it.next().unwrap(), 42);
-    assert_eq!(succ_it.next().unwrap(), 46);
-    assert_eq!(succ_it.next().unwrap(), 49);
-    assert_eq!(succ_it.next().unwrap(), 50);
-    assert_eq!(succ_it.next().unwrap(), 51);
-    assert_eq!(succ_it.next(), None);
+    let correct = vec![10, 38, 42, 46, 49, 50, 51].into_iter();
+
+    assert!(succ_it.eq(correct));
 }
 
 #[test]
 fn test_iteration_on_successors_of_last_node() {
-    let mut graph = build_graph();
+    let graph = build_graph();
     let last_node = graph.num_nodes() - 1;
-    let mut succ_it = graph.successors(last_node).unwrap();
+    let succ_it = graph.successors(last_node).unwrap();
 
-    assert_eq!(succ_it.next().unwrap(), 99979);
-    assert_eq!(succ_it.next().unwrap(), 99980);
-    assert_eq!(succ_it.next().unwrap(), 99981);
-    assert_eq!(succ_it.next().unwrap(), 99982);
-    assert_eq!(succ_it.next().unwrap(), 99983);
-    assert_eq!(succ_it.next().unwrap(), 99984);
-    assert_eq!(succ_it.next().unwrap(), 99985);
-    assert_eq!(succ_it.next().unwrap(), 99986);
-    assert_eq!(succ_it.next().unwrap(), 99987);
-    assert_eq!(succ_it.next().unwrap(), 99988);
-    assert_eq!(succ_it.next().unwrap(), 99989);
-    assert_eq!(succ_it.next().unwrap(), 99990);
-    assert_eq!(succ_it.next().unwrap(), 99998);
-    assert_eq!(succ_it.next().unwrap(), 99999);
-    assert_eq!(succ_it.next(), None);
+    let correct = 
+        vec![99979, 99980, 99981, 99982, 99983, 99984, 99985, 99986, 99987, 99988, 99989, 99990, 99998, 99999].into_iter();
+
+    assert!(succ_it.eq(correct));
 }
 
 #[test]
 fn test_iteration_on_successors_out_of_bounds() {
-    let mut graph = build_graph();
+    let graph = build_graph();
     let succ_it = graph.successors(graph.num_nodes());
     
     assert!(succ_it.is_none());
