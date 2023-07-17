ee36ec9e0f08968fa6cd3c27a88a6afb2848ba3e
caba5 <elabalero@gmail.com>
2023-07-17 20:59:55 +0200

    Implemented Display for EncodingType enum

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,5 @@
+use std::fmt::Display;
+
 use clap::ValueEnum;
 use serde::{Deserialize, Serialize};
 
@@ -60,4 +62,18 @@ pub enum EncodingType {
     GOLOMB,
     SKEWEDGOLOMB,
     UNARY,
+}
+
+impl Display for EncodingType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        f.write_str(match self {
+            EncodingType::GAMMA => "gamma",
+            EncodingType::DELTA => "delta",
+            EncodingType::ZETA => "zeta",
+            EncodingType::NIBBLE => "nibble",
+            EncodingType::GOLOMB => "golomb",
+            EncodingType::SKEWEDGOLOMB => "skewed golomb",
+            EncodingType::UNARY => "unary"
+        })
+    }
 }
