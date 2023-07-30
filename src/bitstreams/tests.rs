f7e9fc708ac2c8f5bdda9e541268a8d36583a4d4
caba5 <elabalero@gmail.com>
2023-07-30 15:08:50 +0200

    Implemented codes tests

--- src/bitstreams/tests.rs
+++ src/bitstreams/tests.rs
@@ -1,6 +1,6 @@
 use std::fs;
 
-use super::BinaryWriterBuilder;
+use super::{BinaryWriterBuilder, BinaryReader};
 
 fn write_unary(writer: &mut BinaryWriterBuilder, x: u64) -> u64 {
     if x < writer.free as u64 {
@@ -59,15 +59,103 @@ fn write_zeta(writer: &mut BinaryWriterBuilder, x: u64, zk: u64) -> u64 {
             {writer.push_bits(x, h * zk + zk)}
 }
 
-#[test]
-fn test_correctness_write_and_read_to_file() {
-    let mut write_builder = BinaryWriterBuilder::new();
-    let mut written_bits = 0;
-
-    for x in 0..1000000 {
-        written_bits += write_gamma(&mut write_builder, x);
+fn read_unary(reader: &mut BinaryReader) -> u64 {
+    if reader.fill < 16 {
+        reader.refill();
     }
 
-    fs::write("temp", write_builder.build().os).unwrap();
+    let mut x = u32::leading_zeros((reader.current as u32) << (32 - reader.fill));
+    if x < reader.fill as u32{
+        reader.read_bits += x as usize + 1;
+        reader.fill -= x as usize + 1;
+        return x as u64;
+    }
+
+    x = reader.fill as u32;
+    let mut read = reader.read();
+
+    if read.is_ok() {
+        reader.current = read.unwrap();
+        while reader.current == 0 && read.is_ok() {
+            x += 8;
+            read = reader.read();
+            if let Ok(r) = read {
+                reader.current = r;
+            }
+        }
+    }
+
+    reader.fill = (63 - u64::leading_zeros(reader.current)) as usize;
+    x += 7 - reader.fill as u32;
+    reader.read_bits += x as usize + 1;
+    x as u64
+}
+
+fn read_gamma(reader: &mut BinaryReader) -> u64 {
+    let msb = read_unary(reader);
+    ((1 << msb) | reader.read_int(msb)) - 1
+}
+
+fn read_delta(reader: &mut BinaryReader) -> u64 {
+    let msb = read_gamma(reader);
+    ((1 << msb) | reader.read_int(msb)) - 1
+}
+
+fn read_zeta(reader: &mut BinaryReader, zk: u64) -> u64 {
+    let unary = read_unary(reader);
+    let left = 1 << (unary * zk);
+    let m = reader.read_int(unary * zk + zk - 1);
+    if m < left {m + left - 1} else {(m << 1) + reader.read_from_current(1) - 1}
+}
+
+fn test_correctness_write_and_read_to_file(code: &str) {
+    let mut write_builder = BinaryWriterBuilder::new();
+
+    for x in 0..100000 {
+        let _ = match code {
+            "UNARY" => write_unary(&mut write_builder, x),
+            "GAMMA" => write_gamma(&mut write_builder, x),
+            "DELTA" => write_delta(&mut write_builder, x),
+            "ZETA" => write_zeta(&mut write_builder, x, 3),
+            _ => unreachable!()
+        };
+    }
+
+    fs::write(code, write_builder.build().os).unwrap();
     
+    let read_file = fs::read(code).unwrap();
+
+    let mut binary_reader = BinaryReader::new(read_file.into_boxed_slice());
+
+    for x in 0..100000 {
+        match code {
+            "UNARY" => assert!(read_unary(&mut binary_reader) == x),
+            "GAMMA" => assert!(read_gamma(&mut binary_reader) == x),
+            "DELTA" => assert!(read_delta(&mut binary_reader) == x),
+            "ZETA" => assert!(read_zeta(&mut binary_reader, 3) == x),
+            _ => unreachable!()
+        };
+    }
+
+    fs::remove_file(code).unwrap();
+}
+
+#[test]
+fn test_unary() {
+    test_correctness_write_and_read_to_file("UNARY");
+}
+
+#[test]
+fn test_gamma() {
+    test_correctness_write_and_read_to_file("GAMMA");
+}
+
+#[test]
+fn test_delta() {
+    test_correctness_write_and_read_to_file("DELTA");
+}
+
+#[test]
+fn test_zeta() {
+    test_correctness_write_and_read_to_file("ZETA");
 }
