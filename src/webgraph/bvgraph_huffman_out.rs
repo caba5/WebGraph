207802fa854577b68ff06a406422d0b17f6a73a9
caba5 <elabalero@gmail.com>
2023-10-05 12:52:08 +0200

    Fixed intervals bug

--- src/webgraph/bvgraph_huffman_out.rs
+++ src/webgraph/bvgraph_huffman_out.rs
@@ -2,7 +2,7 @@ use std::{fs, vec, cmp::Ordering, marker::PhantomData, cell::{RefCell, Cell}, rc
 
 use sucds::{mii_sequences::{EliasFanoBuilder, EliasFano}, Serializable};
 
-use crate::{ImmutableGraph, int2nat, nat2int, plain_webgraph::BVGraphPlain, properties::Properties, utils::{encodings::{UniversalCode, GammaCode, UnaryCode, ZetaCode}, huffman::{Huffman, HuffmanEncoder, Huff}}, BitsLen};
+use crate::{ImmutableGraph, int2nat, nat2int, plain_webgraph::BVGraphPlain, properties::Properties, utils::{encodings::{UniversalCode, GammaCode, UnaryCode, ZetaCode, Huffman}, huffman::HuffmanEncoder}, BitsLen};
 use crate::bitstreams::{BinaryReader, BinaryWriterBuilder};
 
 #[derive(Clone, Eq, PartialEq, Debug, Default)]
@@ -1264,7 +1264,10 @@ impl<
                     
                     prev = self.compression_vectors.left.borrow()[i] + curr_int_len;
                     
-                    _t = GammaCode::write_next(graph_obs, (curr_int_len - self.min_interval_len) as u64, self.zeta_k) as usize;
+                    if let Some(intervals_huff) = intervals_huff {
+                        ///////////////////////////////// HUFFMAN ////////////////////////////////////////
+                        intervals_huff.write(graph_obs, curr_int_len - self.min_interval_len);
+                    }
                 }
                 
                 residual_count = self.compression_vectors.residuals.borrow().len();
@@ -1824,7 +1827,7 @@ impl<
         self
     }
 
-    // Sets the `k` parameter for *zeta*-coding, if present.
+    /// Sets the `k` parameter for *zeta*-coding, if present.
     /// 
     /// # Arguments
     /// 
@@ -1901,56 +1904,4 @@ impl<
             _phantom_out_residual_coding: PhantomData,
         }
     }
-}
-
-
-#[test]
-fn test_huff() {
-    let source = "../../../enwiki-2022";
-    let dest = "../../../huff_test3";
-    let properties_file = fs::File::open(format!("{}.properties", source));
-    let properties_file = properties_file.unwrap_or_else(|_| panic!("Could not find {}.properties", source));
-    let p = java_properties::read(std::io::BufReader::new(properties_file)).unwrap_or_else(|_| panic!("Failed parsing the properties file"));
-
-    let mut props = Properties::from(p);
-
-    assert!(props.nodes as u64 <= u64::MAX, "This version of WebGraph cannot handle graphs with {} (>=2^63) nodes", props.nodes);
-
-    props.window_size = 7;
-    props.max_ref_count = 3;
-    props.min_interval_len = 4;
-
-    let mut bvgraph = BVGraphBuilder::<
-        GammaCode,
-        GammaCode,
-        GammaCode,
-        GammaCode,
-        UnaryCode,
-        ZetaCode,
-        Huff,
-        GammaCode,
-        GammaCode,
-        GammaCode,
-        UnaryCode,
-        Huff,
-    >::new()
-        .set_min_interval_len(props.min_interval_len)
-        .set_max_ref_count(props.max_ref_count)
-        .set_window_size(props.window_size)
-        .set_zeta(props.zeta_k)
-        .set_num_nodes(props.nodes)
-        .set_num_edges(props.arcs)
-        .load_graph(source)
-        .load_offsets(source)
-        .load_outdegrees()
-        .build();
-
-    let comp_time = Instant::now();
-    bvgraph.store(dest).expect("Failed storing the graph");
-    let comp_time = comp_time.elapsed().as_nanos() as f64;
-    println!("compressed the graph in {}ns", comp_time);
-
-    // TODO: divide BVGRAPH into reader and writer
-    // Update properties
-    // Test writing and reading huff
 }
